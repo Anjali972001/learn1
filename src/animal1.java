ba8d65bd28a35dacd4d389288b7c7d2bfeffcd8f
Anjali <anjalikumari972001@gmail.com>
2021-11-18 23:09:19 +0530

    practice

---
+++ src/animal1.java
@@ -0,0 +1,15 @@
+public class animal1 {
+    void eat()
+    {
+        System.out.println("I am eating.");
+    }
+
+    public static void main(String[] args) {
+        animal1 a1 = new animal1();
+        a1.eat();
+        a1.run();
+    }
+    void run(){
+        System.out.println("I am running.");
+    }
+}
