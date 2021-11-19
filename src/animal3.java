5bbf707f4e4898685e7895114268094028120cc2
Anjali <anjalikumari972001@gmail.com>
2021-11-19 09:34:52 +0530

    practice

---
+++ src/animal3.java
@@ -0,0 +1,32 @@
+public class animal3 {
+    void eat()
+    {
+        System.out.println("I am eating.");
+    }
+    void run()
+    {
+        System.out.println("I am running.");
+    }
+    void fly()
+    {
+        System.out.println("I am flying .");
+    }
+
+    public static void main(String[] args) {
+        animal3 dog = new animal3();
+        dog.eat();
+        dog.run();
+
+
+
+    }
+    static class bird
+    {
+        void fly()
+        {
+            System.out.println("I am flying.");
+        }
+
+
+    }
+}
