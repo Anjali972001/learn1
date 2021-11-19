dcafab615262a6441f8b6d9f9160fcdf8c7c44f5
Anjali <anjalikumari972001@gmail.com>
2021-11-19 10:22:18 +0530

    practice

---
+++ src/employee1.java
@@ -0,0 +1,10 @@
+public class employee1 {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        employee1 e1 = new employee1();
+        System.out.println("Employee1: "+e1.name+" "+e1.age);
+
+    }
+}// output : Employee1 : null 0 -----> Default constructor
