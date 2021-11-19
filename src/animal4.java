b756a44a5a539a39dae8eb9a6227e57c78147cfb
Anjali <anjalikumari972001@gmail.com>
2021-11-19 09:54:57 +0530

    practice

---
+++ src/animal4.java
@@ -0,0 +1,11 @@
+public class animal4 {
+    String colour;
+    int age;
+
+    public static void main(String[] args) {
+        animal4 dog = new animal4();
+        dog.colour="Black";
+        dog.age = 3;
+        System.out.println(dog.colour + " "+ dog.age);
+    }
+}// initializing an object by reference variable.
